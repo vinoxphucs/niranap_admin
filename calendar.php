e312a041e6c956fb913a7766b5d5c914505234fa
Nguyễn Phúc <nghphuc1010@gmail.com>
2022-03-10 10:00:30 +0700

    niranap_admin

---
+++ calendar.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+        <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+        <title>NIRAN AP - QUẢN LÝ</title>
+        <!--     Fonts and icons     -->
+        <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
+        <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+        <!-- Nucleo Icons -->
+        <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+        <!-- CSS Files -->
+        <link href="../assets/css/black-dashboard.css?v=1.0.0" rel="stylesheet" />
+        <!-- CSS Just for demo purpose, don't include it in your project -->
+        <link href="../assets/demo/demo.css" rel="stylesheet" />
+    </head>
+
+    <body class="">
+    <div class="wrapper ">
+  <div class="sidebar" data-color="purple" data-background-color="white">
+    <div class="logo">
+      <a href="" class="simple-text logo-mini">AP</a>
+
+      <a href="" class="simple-text logo-normal">NIRAN</a>
+    </div>
+
+    <div class="sidebar-wrapper">
+      <ul class="nav">
+        <li class="nav-item active  ">
+            <a class="nav-link" href="index.php">
+                <i class="tim-icons icon-app"></i>
+                <p>Dashboard</p>
+            </a>
+        </li>
+        <li class="nav-item active  ">
+            <a class="nav-link" href="member.php">
+                <i class="tim-icons icon-molecule-40"></i>
+                <p>THÀNH VIÊN</p>
+            </a>
+        </li>
+        <li class="nav-item active  ">
+            <a class="nav-link" href="profile.php">
+                <i class="tim-icons icon-alert-circle-exc"></i>
+                <p>HỒ SƠ</p>
+            </a>
+        </li>
+        <li class="nav-item active  ">
+            <a class="nav-link" href="money.php">
+                <i class="tim-icons icon-money-coins"></i>
+                <p>TIỀN CỦA TÔI</p>
+            </a>
+        </li>
+        <li class="nav-item active  ">
+            <a class="nav-link" href="calendar.php">
+                <i class="tim-icons icon-time-alarm"></i>
+                <p>LỊCH RA TRUYỆN</p>
+            </a>
+        </li>
+         <!-- your sidebar here -->
+      </ul>
+    </div>
+  </div>
+  <div class="main-panel">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top " aria-label="">
+      <div class="container-fluid">
+        <div class="navbar-wrapper">
+
+          <a class="navbar-brand" href="#">AP Dashboard</a>
+        </div>
+        <div class="collapse navbar-collapse justify-content-end">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <i class="tim-icons icon-bell-55"></i>  Notifications
+              </a>
+            </li>
+             <!-- your navbar here -->
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- End Navbar -->
+    <div class="content">
+      <div class="container-fluid">
+         <!-- your content here -->
+        <nav aria-label="breadcrumb" role="navigation">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Lịch ra truyện</li>
+            </ol>
+        </nav>
+      </div>
+    </div>
+    <footer class="footer">
+      <div class="container-fluid">
+        <nav class="float-left" aria-label="">
+          <ul>
+              <li>
+                  <a href="">
+                      Niran AP
+                  </a>
+              </li>
+          </ul>
+        </nav>
+        <div class="copyright float-right">
+            &copy;
+            <script>
+                document.write(new Date().getFullYear())
+            </script>, <a href="" target="_blank">Niran AP</a>
+        </div>
+
+         <!-- your footer here -->
+      </div>
+    </footer>
+  </div>
+</div>
+
+        <!--   Core JS Files   -->
+        <script src="../assets/js/core/jquery.min.js"></script>
+        <script src="../assets/js/core/popper.min.js"></script>
+        <script src="../assets/js/core/bootstrap.min.js"></script>
+        <script src="../assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+        <!--  Google Maps Plugin    -->
+        <!-- Place this tag in your head or just before your close body tag. -->
+        <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+        <!-- Chart JS -->
+        <script src="../assets/js/plugins/chartjs.min.js"></script>
+        <!--  Notifications Plugin    -->
+        <script src="../assets/js/plugins/bootstrap-notify.js"></script>
+        <!-- Control Center for Black Dashboard: parallax effects, scripts for the example pages etc -->
+        <script src="../assets/js/black-dashboard.min.js?v=1.0.0"></script><!-- Black Dashboard DEMO methods, don't include it in your project! -->
+        <script src="../assets/demo/demo.js"></script>
+        <script src="https://cdn.trackjs.com/agent/v3/latest/t.js"></script>
+    </body>
+
+</html>
